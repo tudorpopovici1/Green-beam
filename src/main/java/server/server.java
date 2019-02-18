3a8c37e7cd2b8b71621f374e7ffd4288e33e9a6e
irtazahashmi <irtazahashmi@gmail.com>
2019-02-18 11:27:00 +0100

    First program to test intelliJ

---
+++ src/main/java/server/server.java
@@ -0,0 +1,12 @@
+package server;
+
+/*
+A class to have make a server for our application.
+ */
+public class server {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
