9ab8477ca16b0662e0d4839c84f23c587ed73f5c
Tudor <t.popovici@student.tudelft.nl>
2019-02-20 18:22:10 +0100

    First Client application

---
+++ src/main/java/client/Application.java
@@ -0,0 +1,40 @@
+package client;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class Application{
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[])
+    {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder)
+    {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception
+    {
+        return args -> {
+          Message message = restTemplate.getForObject("http://localhost:8080/greeting", Message.class);
+          log.info(message.toString());
+        };
+    }
+}
