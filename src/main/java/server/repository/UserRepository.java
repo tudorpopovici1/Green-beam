5691c346dd2aeabbb5ac4e5e4d0a052e668bf88e
Tudor <t.popovici@student.tudelft.nl>
2019-02-22 20:22:14 +0100

    Added a method to retrieve all friends of a specific user

--- src/main/java/server/repository/UserRepository.java
+++ src/main/java/server/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package server.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import server.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    @Query("SELECT u FROM user_data u " +
+            "JOIN friends_relationship f ON(u.id = f.related_user_id) " +
+            "WHERE f.relating_user_id = ?1")
+    List<Users> findAllFriendsUser(Long userId);
 }
