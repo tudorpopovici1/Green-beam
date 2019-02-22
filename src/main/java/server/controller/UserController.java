67dcdc31e0f9d37d605b0fa841479c6c4072d2bc
Tudor <t.popovici@student.tudelft.nl>
2019-02-22 20:22:27 +0100

    Added another path to retrieve friends of user

--- src/main/java/server/controller/UserController.java
+++ src/main/java/server/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.model.*;
@@ -19,10 +20,19 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    // Returns all the users in the database.
     @GetMapping("/user/all")
     public List<Users> getUsers()
     {
         return userRepository.findAll();
+    }
+
+
+    // Return a list of all the friends of user with id given as parameter.
+    @GetMapping("/user/allfriends/{id}")
+    public List<Users> getFriendsUser(@PathVariable("id") Long id)
+    {
+        return userRepository.findAllFriendsUser(id);
     }
 
 }
