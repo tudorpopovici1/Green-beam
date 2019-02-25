c62a5ffdb9c2e1d458b8ad1c767ba1d99f7668c1
Tudor <t.popovici@student.tudelft.nl>
2019-02-25 04:41:56 +0100

    Added a new method, updated the previous query

--- src/main/java/server/repository/UserRepository.java
+++ src/main/java/server/repository/UserRepository.java
@@ -6,13 +6,14 @@ import server.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-    @Query("SELECT u FROM user_data u " +
+    @Query(value = "SELECT new server.model.FriendsUserResp(u.username, u.dateOfBirth, u.firstName, u.lastName) FROM user_data u " +
             "JOIN friends_relationship f ON(u.id = f.related_user_id) " +
             "WHERE f.relating_user_id = ?1")
-    List<Users> findAllFriendsUser(Long userId);
+    List<FriendsUserResp> findAllFriendsUser(Long userId);
+
+    Users findByUsername(String username);
 }
