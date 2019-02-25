2578a3c0282062f733ae6ad1b24bf0cca5f6fe0e
Tudor <t.popovici@student.tudelft.nl>
2019-02-25 04:42:22 +0100

    Added exception handling

--- src/main/java/server/controller/UserController.java
+++ src/main/java/server/controller/UserController.java
@@ -1,17 +1,13 @@
 package server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import server.exception.CustomException;
 import server.model.*;
 import server.repository.UserRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest")
@@ -27,10 +23,28 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @PostMapping(value = "/save/user")
+    public Users saveUser(@RequestBody Users user) throws CustomException {
+        if(userRepository.findByUsername(user.getUsername()) != null)
+        {
+            throw new CustomException("Username: " + user.getUsername() + " is already in use..");
+        }
+
+        return userRepository.save(user);
+    }
+
+    @GetMapping(value = "/user/{id}")
+    public Optional<Users> getUser(@PathVariable("id") Long id) throws CustomException {
+
+       if(!userRepository.findById(id).isPresent())
+            throw new CustomException("The user is not available");
+
+        return userRepository.findById(id);
+    }
 
     // Return a list of all the friends of user with id given as parameter.
     @GetMapping("/user/allfriends/{id}")
-    public List<Users> getFriendsUser(@PathVariable("id") Long id)
+    public List<FriendsUserResp> getFriendsUser(@PathVariable("id") Long id)
     {
         return userRepository.findAllFriendsUser(id);
     }
