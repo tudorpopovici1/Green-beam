40d5bcf26471fff83f636c65d5494126bd796549
Tudor <t.popovici@student.tudelft.nl>
2019-02-26 02:58:16 +0100

    Bean to represent the JSON object having username and password in order to login

---
+++ src/main/java/server/model/AuthenticateUser.java
@@ -0,0 +1,31 @@
+package server.model;
+
+public class AuthenticateUser {
+
+    private String username;
+    private String password;
+
+    public AuthenticateUser() {
+    }
+
+    public AuthenticateUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
