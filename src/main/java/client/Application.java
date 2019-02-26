d9a331a3c7c40fcbd0632ea2decd93595717a094
Tudor <t.popovici@student.tudelft.nl>
2019-02-26 02:58:34 +0100

    Reformatted main using enums

--- src/main/java/client/Application.java
+++ src/main/java/client/Application.java
@@ -4,34 +4,51 @@ package client;
 import client.services.MessageService;
 import client.services.UserService;
 import org.springframework.web.client.RestTemplate;
-import server.exception.CustomException;
+import server.model.AuthenticateUser;
+import server.model.Users;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 //@SpringBootApplication
 public class Application {
 
-    private static final String GET_MESSAGE = "http://localhost:8080/greeting";
-    private static String GET_USER = "http://localhost:8080/rest/user";
-    private static final String GET_ALL_USERS = "http://localhost:8080/rest/user/all";
-    private static final String ADD_USER = "http://localhost:8080/rest/save/user";
-    private static final String GET_USER_FRIENDS = "http://localhost:8080/rest/user/allfriends";
-
     private static final RestTemplate restTemplate = new RestTemplate();
 
+    private static final String ADD_USER = URL.ADD_USER.getUrl();
+    private static final String GET_USER_FRIENDS = URL.GET_USER_FRIENDS.getUrl();
+    private static final String GET_USER = URL.GET_USER.getUrl();
+    private static final String AUTH_USER = URL.AUTH_USER.getUrl();
+    private static final String GET_ALL_USERS = URL.GET_ALL_USERS.getUrl();
 
-    public static void main(final String[] args) throws CustomException {
+
+    public static void main(final String[] args) {
 
         UserService userService = new UserService();
 
-        MessageService messageService = new MessageService();
+        String pattern = "dd-MM-yyyy";
+        DateFormat dateFormat = new SimpleDateFormat(pattern);
 
-        Message msg = messageService.getMessage(restTemplate, GET_MESSAGE);
+        try {
+
+            Date dob = dateFormat.parse("20-12-1999");
+            Users user = new Users(null, "demo", "123", "TestName", "LastName", "RO", "123@gmail.com", dob);
+
+            userService.addUser(restTemplate, ADD_USER,user);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
 
         userService.getUser(restTemplate, GET_USER, 15L);
 
-        userService.addUser(restTemplate, ADD_USER,"test8");
-
         userService.getUserFriends(restTemplate, GET_USER_FRIENDS, 0L);
+
+        userService.authUser(restTemplate, AUTH_USER, new AuthenticateUser("test15", "123"));
 
     }
 }
