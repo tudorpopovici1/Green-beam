eb09af9fbf4985d5d03dc0a5087335241196db5f
Tudor <t.popovici@student.tudelft.nl>
2019-02-27 03:41:56 +0100

    Added checking method for correct user

--- src/main/java/server/controller/UserController.java
+++ src/main/java/server/controller/UserController.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import server.exception.BadCredentialsException;
+import server.exception.DbException;
 import server.exception.ResourceNotFoundException;
 import server.exception.UserAlreadyRegistered;
 import server.model.*;
 import server.repository.UserRepository;
+import server.security.JwtValidator;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +24,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    JwtValidator jwtValidator;
+
     // Returns all the users in the database.
     @GetMapping("/user/all")
     public List<Users> getUsers()
@@ -27,10 +35,16 @@ public class UserController {
     }
 
     @PostMapping(value = "/save/user")
-    public Users saveUser(@RequestBody Users user) throws UserAlreadyRegistered {
+    public Users saveUser(@RequestBody Users user) throws UserAlreadyRegistered, DbException {
+
         if(userRepository.findByUsername(user.getUsername()) != null)
         {
             throw new UserAlreadyRegistered("Username: " + user.getUsername() + " is already in use..");
+        }
+
+        if(userRepository.findByEmail(user.getUsername()) != null)
+        {
+            throw new UserAlreadyRegistered("Email is in use...");
         }
 
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
@@ -41,44 +55,47 @@ public class UserController {
     }
 
     @GetMapping(value = "/user/{id}")
-    public Optional<Users> getUser(@PathVariable("id") Long id) throws ResourceNotFoundException {
+    public FriendsUserResp getUser(HttpServletRequest request, @PathVariable("id") Long id) throws ResourceNotFoundException, BadCredentialsException {
+
+        if(!isCorrectUser(request, id))
+            throw new BadCredentialsException("Nice try!");
 
        if(!userRepository.findById(id).isPresent())
             throw new ResourceNotFoundException("The user is not available");
 
-        return userRepository.findById(id);
+        return userRepository.findSpecificUserById(id);
     }
 
     // Return a list of all the friends of user with id given as parameter.
     @GetMapping("/user/allfriends/{id}")
-    public List<FriendsUserResp> getFriendsUser(@PathVariable("id") Long id)
-    {
+    public List<FriendsUserResp> getFriendsUser(HttpServletRequest request, @PathVariable("id") Long id) throws BadCredentialsException {
+
+        if(!isCorrectUser(request, id))
+            throw new BadCredentialsException("Bad credentials");
+
         return userRepository.findAllFriendsUser(id);
     }
 
-
-    //Authenticate users.
-    @PostMapping("/user/auth")
-    public void authUser(@RequestBody AuthenticateUser authenticateUser) throws BadCredentialsException {
-
-        String username = authenticateUser.getUsername();
-        String password = authenticateUser.getPassword();
-
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-
-        String retrievedPassword = userRepository.findUserPassword(username);
-
-        System.out.println(retrievedPassword);
-
-        if(userRepository.findByUsername(username) == null || !bCryptPasswordEncoder.matches(password, retrievedPassword))
-            throw new BadCredentialsException("Incorrect username or password");
-
-    }
-
     // Handle All other URLS.
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public void redirectEverythingOtherThanTest()throws ResourceNotFoundException{
         throw new ResourceNotFoundException();
+    }
+
+    private boolean isCorrectUser(HttpServletRequest request, Long id){
+
+        String token = request.getHeader("Authorisation").substring(6);
+
+        JwtUser jwtUser = jwtValidator.validate(token);
+
+        System.out.println(token);
+        if(jwtUser != null)
+            System.out.println(jwtUser);
+
+        if(jwtUser == null || id != jwtUser.getId())
+            return false;
+
+        return true;
     }
 
 }
