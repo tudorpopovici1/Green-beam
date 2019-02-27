0dbe11b0958ceb7176601d1c1ff9eed7b083e690
Tudor <t.popovici@student.tudelft.nl>
2019-02-27 03:42:26 +0100

    Changed some parameters of a constructor

--- src/test/java/serverTesting/userServiceTest.java
+++ src/test/java/serverTesting/userServiceTest.java
@@ -1,6 +1,5 @@
 package serverTesting;
 
-import client.services.MessageService;
 import client.services.UserService;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,13 +11,15 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
-import server.exception.CustomException;
+import server.exception.ResourceNotFoundException;
 import server.model.Users;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(MockitoJUnitRunner.class)
 public class userServiceTest {
@@ -30,18 +31,19 @@ public class userServiceTest {
     UserService userService = new UserService();
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testGetUser() throws CustomException {
+    public void testGetUser() throws ResourceNotFoundException {
         String pattern = "dd-MM-yyyy";
         DateFormat dateFormat = new SimpleDateFormat(pattern);
 
-        try{
+        try {
             Date dob = dateFormat.parse("20-12-1999");
-            Users user = new Users(0L, "username", "pwd", "fname", "lname", "country", "email", dob);
+            Users user = new Users(0L, "username", "pwd", "fname",
+                    "lname", "country", "email", dob, "user");
 
             Mockito
                     .when(restTemplate.getForObject("http://localhost:8080/rest/user/0", Users.class))
@@ -50,11 +52,18 @@ public class userServiceTest {
             Users result = userService.getUser(restTemplate, "http://localhost:8080/rest/user", 0L);
 
             Assert.assertEquals(user, result);
-        }
-        catch(ParseException e)
-        {
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
 
+    @Test(expected = ResourceNotFoundException.class)
+    public void getUserExceptionThrown() throws ResourceNotFoundException {
+
+        Mockito
+                .when(restTemplate.getForObject("http://localhost:8080/rest/user", Users.class))
+                .thenThrow(new ResourceNotFoundException("The user is not available"));
+
+        Users user = userService.getUser(restTemplate, "http://localhost:8080/rest/user", 16l);
+    }
 }
