0f8e86dc710c0060d448f82c9d8b328fb7266982
Tudor <t.popovici@student.tudelft.nl>
2019-02-27 03:39:17 +0100

    A generator for JWT access tokens

---
+++ src/main/java/server/security/JwtGenerator.java
@@ -0,0 +1,24 @@
+package server.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import server.model.JwtUser;
+
+@Component
+public class JwtGenerator {
+
+    public String generate(JwtUser jwtUser) {
+
+        Claims claims = Jwts.claims()
+                .setSubject(jwtUser.getUserName());
+        claims.put("userId", String.valueOf(jwtUser.getId()));
+        claims.put("role", jwtUser.getRole());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, "secret")
+                .compact();
+    }
+}
