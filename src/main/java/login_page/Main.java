a40204169b87098abee429730578569bab5656cb
Tudor <t.popovici@student.tudelft.nl>
2019-02-28 03:49:39 +0100

    Updated main class, added new back ims

--- src/main/java/login_page/Main.java
+++ src/main/java/login_page/Main.java
@@ -20,8 +20,9 @@ public class Main extends Application {
         try {
             URL url = new File("src/main/java/login_page/Login.fxml").toURI().toURL();
             Parent root = FXMLLoader.load(url);
-            Scene scene = new Scene(root, 950, 600);
+            Scene scene = new Scene(root, 900, 600);
             primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
             primaryStage.show();
         }
         catch (Exception e){
