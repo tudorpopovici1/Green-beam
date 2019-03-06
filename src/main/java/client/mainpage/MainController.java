218eb388401c6620807372d475a35d4cff267e7b
irtazahashmi <irtazahashmi@gmail.com>
2019-03-06 04:30:12 +0100

    Animation added between windows.

--- src/main/java/client/mainpage/MainController.java
+++ src/main/java/client/mainpage/MainController.java
@@ -1,9 +1,11 @@
 package client.mainpage;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
-import javafx.event.ActionEvent;
-
+import javafx.util.Duration;
 
 
 public class MainController {
@@ -28,46 +30,54 @@ public class MainController {
 
 
     public void mainPage(ActionEvent event) {
+        animate(mainWindow);
         mainWindow.toFront();
         mainWindow.setStyle("-fx-background-color: #FFFFFFFF");
     }
 
     public void progressPage(ActionEvent event) {
         progressWindow.setVisible(true);
+        animate(progressWindow);
         progressWindow.toFront();
         progressWindow.setStyle("-fx-background-color: #000000");
     }
 
+
     public void profileWindow(ActionEvent event) {
         profileWindow.setVisible(true);
+        animate(profileWindow);
         profileWindow.toFront();
         profileWindow.setStyle("-fx-background-color: blue");
     }
 
     public void aboutUsWindow(ActionEvent event) {
         aboutUsWindow.setVisible(true);
+        animate(aboutUsWindow);
         aboutUsWindow.toFront();
         aboutUsWindow.setStyle("-fx-background-color: grey");
     }
 
     public void settingsWindow(ActionEvent event) {
         settingsWindow.setVisible(true);
+        animate(settingsWindow);
         settingsWindow.toFront();
         settingsWindow.setStyle("-fx-background-color: orange");
     }
 
     public void logoutWindow(ActionEvent event) {
         logoutWindow.setVisible(true);
+        animate(logoutWindow);
         logoutWindow.toFront();
         logoutWindow.setStyle("-fx-background-color: red");
     }
 
-
-
-
-
-
-
-
-
+    public void animate(Pane pane) {
+        ResizeHeightTranslation rht = new ResizeHeightTranslation
+                (Duration.millis(500), pane, pane.getMinHeight());
+        FadeTransition ft = new FadeTransition(Duration.millis(500), pane);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        SequentialTransition pt = new SequentialTransition(rht, ft);
+        pt.play();
+    }
 }
---
+++ src/main/java/client/mainpage/ResizeHeightTranslation.java
@@ -0,0 +1,26 @@
+package client.mainpage;
+
+import javafx.animation.Transition;
+import javafx.scene.layout.Region;
+import javafx.util.Duration;
+
+public class ResizeHeightTranslation extends Transition {
+
+    protected Region region;
+    protected double startHeight;
+    protected double newHeight;
+    protected double heightDiff;
+
+    public ResizeHeightTranslation( Duration duration, Region region, double newHeight ) {
+        setCycleDuration(duration);
+        this.region = region;
+        this.newHeight = newHeight;
+        this.startHeight = region.getHeight();
+        this.heightDiff = newHeight - startHeight;
+    }
+
+    @Override
+    protected void interpolate(double fraction) {
+        region.setMinHeight( startHeight + ( heightDiff * fraction ) );
+    }
+}
