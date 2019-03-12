69e5708eb52d601388cdfd78e86be80d8534587f
MerelSusanna <merelsusanna@gmail.com>
2019-03-12 15:41:26 +0100

    getters and setters tested JwtUser

---
+++ src/test/java/server/model/JwtAuthenticationTokenTest.java
@@ -0,0 +1,29 @@
+package server.model;
+
+import org.junit.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JwtAuthenticationTokenTest {
+
+//    JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken();
+//
+//    @org.junit.jupiter.api.Test
+//    public void getTokenTest() {
+//        String token = "abc";
+//        jwtAuthenticationToken.setToken(token);
+//        String expected = "abc";
+//        Assert.assertEquals(jwtAuthenticationToken.getToken(), expected);
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    public void setTokenTest() {
+//        String token = "abc";
+//        jwtAuthenticationToken.setToken(token);
+//        String setToken = jwtAuthenticationToken.getToken();
+//        String expected = "abc";
+//        Assert.assertEquals(setToken, expected);
+//    }
+
+
+}
