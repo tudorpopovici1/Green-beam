003e5a974b1ca442d67b831e13a48d810cedb693
Maria <maria.kusmierowska@yahoo.com>
2019-03-14 20:30:41 +0100

    Merge remote-tracking branch 'origin/Deploy' into Deploy
    # Conflicts:
    #	OOP-Project.iml
    #	src/main/java/server/model/AuthenticateUser.java

---
+++ src/test/java/server/model/EmissionReductionsTest.java
@@ -0,0 +1,93 @@
+package server.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmissionReductionsTest {
+    EmissionReductions emissionReduction=new EmissionReductions();
+    @Test
+    public void getLocalProduceEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setLocalProduceEmission(emission1);
+        float setLocalProduceEmission = emissionReduction.getLocalProduceEmission();
+        float expected = 1234;
+        Assert.assertEquals(setLocalProduceEmission, expected);
+    }
+    @Test
+    public void setLocalProduceEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setLocalProduceEmission(emission1);
+        float setLocalProduceEmission = emissionReduction.getLocalProduceEmission();
+        float expected = 1234;
+        Assert.assertEquals(setLocalProduceEmission, expected);
+    }
+
+    @Test
+    public void setPublicTransportationEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setPublicTransportationEmission(emission1);
+        float setPublicTransportationEmission = emissionReduction.getPublicTransportationEmission();
+        float expected = 1234;
+        Assert.assertEquals(setPublicTransportationEmission, expected);
+    }
+    @Test
+    public void getPublicTransportationEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setPublicTransportationEmission(emission1);
+        float setPublicTransportationEmission = emissionReduction.getPublicTransportationEmission();
+        float expected = 1234;
+        Assert.assertEquals(setPublicTransportationEmission, expected);
+    }
+    @Test
+    public void setVegetarianMealEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setVegetarianMealEmission(emission1);
+        float setVegetarianMealEmission = emissionReduction.getVegetarianMealEmission();
+        float expected = 1234;
+        Assert.assertEquals(setVegetarianMealEmission, expected);
+    }
+    @Test
+    public void getVegetarianMealEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setVegetarianMealEmission(emission1);
+        float setVegetarianMealEmission = emissionReduction.getVegetarianMealEmission();
+        float expected = 1234;
+        Assert.assertEquals(setVegetarianMealEmission, expected);
+    }
+    @Test
+    public void getLoweringTemperatureEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setLoweringTemperatureEmission(emission1);
+        float setLoweringTemperatureEmission= emissionReduction.getLoweringTemperatureEmission();
+        float expected = 1234;
+        Assert.assertEquals(setLoweringTemperatureEmission, expected);
+    }
+
+    @Test
+    public void setLoweringTemperatureEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setLoweringTemperatureEmission(emission1);
+        float setLoweringTemperatureEmission= emissionReduction.getLoweringTemperatureEmission();
+        float expected = 1234;
+        Assert.assertEquals(setLoweringTemperatureEmission, expected);
+    }
+    @Test
+    public void setRideBikeEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setRideBikeEmission(emission1);
+        float setRideBikeEmission= emissionReduction.getRideBikeEmission();
+        float expected = 1234;
+        Assert.assertEquals(setRideBikeEmission, expected);
+    }
+    @Test
+    public void getRideBikeEmissionTest() {
+        float emission1 = 1234;
+        emissionReduction.setRideBikeEmission(emission1);
+        float setRideBikeEmission= emissionReduction.getRideBikeEmission();
+        float expected = 1234;
+        Assert.assertEquals(setRideBikeEmission, expected);
+    }
+
+}
