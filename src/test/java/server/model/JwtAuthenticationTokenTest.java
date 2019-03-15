e3c3dcea709ab803b35a1d6a4f8d03a885cff5ae
irtazahashmi <irtazahashmi@gmail.com>
2019-03-15 14:26:16 +0100

    Tests fixed.

--- src/test/java/server/model/JwtAuthenticationTokenTest.java
+++ src/test/java/server/model/JwtAuthenticationTokenTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class JwtAuthenticationTokenTest {
 
 
-    JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken("");
+    JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(null);
 
     
     @Test
