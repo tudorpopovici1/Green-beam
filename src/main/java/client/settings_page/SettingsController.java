684c48bdd627715d883f24fb618ea440ec7bdadd
Sever Latysov <sever.latysov@gmail.com>
2019-03-17 13:47:56 +0100

    I have to commit this skeleton befor ethe actual code, because Intellij doesn't let me do it any other way

---
+++ src/main/java/client/settings_page/SettingsController.java
@@ -0,0 +1,27 @@
+package client.settings_page;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.Alert.AlertType;
+
+
+public class SettingsController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+}
