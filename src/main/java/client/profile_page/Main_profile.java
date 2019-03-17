a284534ede4677e58c5631b826d1f0f1b01daaab
Sever Latysov <sever.latysov@gmail.com>
2019-03-17 22:20:34 +0100

    Basic profile page initiation.

--- src/main/java/client/profile_page/Main_profile.java
+++ src/main/java/client/profile_page/Main_profile.java
@@ -1,4 +1,32 @@
 package client.profile_page;
 
-public class Main_profile {
+import java.net.URL;
+import java.io.File;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class Main_profile extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            URL url = new File(
+                    "src/main/java/client/profile_page/profile.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(url);
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 709, 700));
+            primaryStage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
+
--- src/main/java/client/profile_page/ProfileController.java
+++ src/main/java/client/profile_page/ProfileController.java
@@ -1,4 +1,26 @@
 package client.profile_page;
 
-public class ProfileController {
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.Alert.AlertType;
+
+public class ProfileController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
 }
