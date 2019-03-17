47e11188faace40567878abd5ad4cae2cc25bc87
irtazahashmi <irtazahashmi@gmail.com>
2019-03-18 00:55:46 +0100

    Achievement tests.

--- src/test/java/server/model/AchievementsTest.java
+++ src/test/java/server/model/AchievementsTest.java
@@ -6,6 +6,25 @@ import org.junit.Test;
 public class AchievementsTest {
 
     Achievements achievement = new Achievements();
+    Achievements achievement2 = new Achievements();
+
+    @Test
+    public void getIdTest() {
+        long id = 1234L;
+        achievement.setId(id);
+        long setId = achievement.getId();
+        long expected = 1234L;
+        Assert.assertEquals(setId, expected);
+    }
+
+    @Test
+    public void setIdTest() {
+        long id = 1234L;
+        achievement.setId(id);
+        long setId = achievement.getId();
+        long expected = 1234L;
+        Assert.assertEquals(setId, expected);
+    }
 
 
     @Test
@@ -45,4 +64,65 @@ public class AchievementsTest {
         Assert.assertEquals(setAchievementId, expected);
     }
 
+    @Test
+    public void toStringTest() {
+        achievement.setId(123L);
+        achievement.setUserId(456L);
+        achievement.setAchievementId(789L);
+        String expected = "Achievements{id=123, userId=456, achievementId=789}";
+        Assert.assertEquals(expected, achievement.toString());
+    }
+
+    @Test
+    public void equalsMethodSameObjectTest() {
+        achievement.setId(123L);
+        achievement.setUserId(456L);
+        achievement.setAchievementId(789L);
+        achievement2 = achievement;
+        Assert.assertEquals(achievement, achievement2);
+    }
+
+    @Test
+    public void equalsMethodDifferentObjectTest() {
+        achievement.setId(123L);
+        achievement.setUserId(456L);
+        achievement.setAchievementId(789L);
+        achievement2.setId(123L);
+        achievement2.setUserId(456L);
+        achievement2.setAchievementId(789L);
+        Assert.assertEquals(achievement, achievement2);
+    }
+
+    @Test
+    public void equalsMethodDifferentIdTest() {
+        achievement.setId(1234L);
+        achievement.setUserId(456L);
+        achievement.setAchievementId(789L);
+        achievement2.setId(123L);
+        achievement2.setUserId(456L);
+        achievement2.setAchievementId(789L);
+        Assert.assertNotEquals(achievement, achievement2);
+    }
+
+    @Test
+    public void equalsMethodDifferentUserIdTest() {
+        achievement.setId(123L);
+        achievement.setUserId(456L);
+        achievement.setAchievementId(789L);
+        achievement2.setId(123L);
+        achievement2.setUserId(4567L);
+        achievement2.setAchievementId(789L);
+        Assert.assertNotEquals(achievement, achievement2);
+    }
+
+    @Test
+    public void equalsMethodDifferentAchievementIdTest() {
+        achievement.setId(123L);
+        achievement.setUserId(456L);
+        achievement.setAchievementId(789L);
+        achievement2.setId(123L);
+        achievement2.setUserId(456L);
+        achievement2.setAchievementId(7899L);
+        Assert.assertNotEquals(achievement, achievement2);
+    }
 }
