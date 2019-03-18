68ecfdce5a3d33e19b1d88728dd6666576cacec2
Sever Latysov <sever.latysov@gmail.com>
2019-03-18 12:10:23 +0100

    Designing the side panel.

---
+++ src/main/java/client/profile_page/SidePanelController.java
@@ -0,0 +1,34 @@
+package client.profile_page;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.Alert.AlertType;
+
+
+public class SidePanelController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+}
+
+
