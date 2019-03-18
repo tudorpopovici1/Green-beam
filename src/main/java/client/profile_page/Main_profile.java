5bfa40bd43d0a7a46051c4de8e571b5c7d42bd9f
Sever Latysov <sever.latysov@gmail.com>
2019-03-18 23:58:55 +0100

    Adding a drawer to the profile page.

--- src/main/java/client/profile_page/Main_profile.java
+++ src/main/java/client/profile_page/Main_profile.java
@@ -17,21 +17,21 @@ public class Main_profile extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-//            URL url = new File(
-//                    "src/main/java/client/profile_page/profile.fxml").toURI().toURL();
-//            Parent root = FXMLLoader.load(url);
-//            primaryStage.setTitle("Hello World");
-//            primaryStage.setScene(new Scene(root, 709, 700));
-//            primaryStage.setResizable(false);
-//            primaryStage.show();
-
-            URL bruh = new File(
-                    "src/main/java/client/profile_page/side_panel.fxml").toURI().toURL();
-            Parent broot = FXMLLoader.load(bruh);
+            URL url = new File(
+                    "src/main/java/client/profile_page/profile.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(url);
             primaryStage.setTitle("Hello World");
-            primaryStage.setScene(new Scene(broot, 200, 700));
+            primaryStage.setScene(new Scene(root, 709, 700));
             primaryStage.setResizable(false);
             primaryStage.show();
+
+//            URL bruh = new File(
+//                    "src/main/java/client/profile_page/side_panel.fxml").toURI().toURL();
+//            Parent broot = FXMLLoader.load(bruh);
+//            primaryStage.setTitle("Hello World");
+//            primaryStage.setScene(new Scene(broot, 200, 700));
+//            primaryStage.setResizable(false);
+//            primaryStage.show();
         }
         catch (Exception e){
             e.printStackTrace();
--- src/main/java/client/profile_page/ProfileController.java
+++ src/main/java/client/profile_page/ProfileController.java
@@ -1,10 +1,17 @@
 package client.profile_page;
 
+import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.fxml.Initializable;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
@@ -12,7 +19,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
@@ -23,8 +30,19 @@ public class ProfileController implements Initializable {
     @FXML
     private JFXHamburger hamburger;
 
+    @FXML
+    private JFXDrawer drawer;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+//        try {
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/java/client/profile_page/side_panel.fxml"));
+//            VBox box = loader.load();
+//            drawer.setSidePane(box);
+//        } catch (IOException ex) {
+//            Logger.getLogger(ProfileController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
 
         System.out.println("View is now loaded!");
         HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
@@ -32,6 +50,12 @@ public class ProfileController implements Initializable {
         hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
             transition.setRate(transition.getRate() * -1);
             transition.play();
+
+            if (drawer.isOpened()) {
+                drawer.close();
+            } else {
+                drawer.open();
+            }
         });
 
     }
