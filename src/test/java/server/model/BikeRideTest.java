5a8ba905c9e4832d780fb195fede3dc51e383338
MerelSusanna <merelsusanna@gmail.com>
2019-03-19 17:01:04 +0100

    added the get/set tests for BikeRide

--- src/test/java/server/model/BikeRideTest.java
+++ src/test/java/server/model/BikeRideTest.java
@@ -1,6 +1,60 @@
 package server.model;
 
+import org.junit.Assert;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BikeRideTest {
 
+    BikeRide bike = new BikeRide(0f, 0f, "");
 
+    @Test
+    public void getNumberOfMilesTest() {
+        bike.setNumberOfMiles(123);
+        float numberOfMiles = bike.getNumberOfMiles();
+        float expected = 123;
+        Assert.assertEquals(expected, numberOfMiles, 0);
+    }
+
+    @Test
+    public void setNumberOfMilesTest() {
+        bike.setNumberOfMiles(123);
+        float numberOfMiles = bike.getNumberOfMiles();
+        float expected = 123;
+        Assert.assertEquals(expected, numberOfMiles, 0);
+    }
+
+    @Test
+    public void getCarMileageTest() {
+        bike.setCarMileage(123);
+        float carMileage = bike.getCarMileage();
+        float expected = 123;
+        Assert.assertEquals(expected, carMileage, 0);
+    }
+
+    @Test
+    public void setCarMileageTest() {
+        bike.setCarMileage(123);
+        float carMileage = bike.getCarMileage();
+        float expected = 123;
+        Assert.assertEquals(expected, carMileage, 0);
+    }
+
+    @Test
+    public void getFuelTypeTest() {
+        bike.setFuelType("Diesel");
+        String fuelType = bike.getFuelType();
+        String expected = "Diesel";
+        Assert.assertEquals(expected, fuelType);
+    }
+
+    @Test
+    public void setFuelTypeTest() {
+        bike.setFuelType("Diesel");
+        String fuelType = bike.getFuelType();
+        String expected = "Diesel";
+        Assert.assertEquals(expected, fuelType);
+    }
 }
+
