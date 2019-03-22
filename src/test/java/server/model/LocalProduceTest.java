d0437fd7ac66392ee6abcad37ea2dea5230773b2
MerelSusanna <merelsusanna@gmail.com>
2019-03-22 20:29:48 +0100

    added the test for LocalProduce

--- src/test/java/server/model/LocalProduceTest.java
+++ src/test/java/server/model/LocalProduceTest.java
@@ -1,88 +1,90 @@
-//package server.model;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class LocalProduceTest {
-//
-//    LocalProduce localProduce = new LocalProduce("", "");
-//    LocalProduce localProduce2 = new LocalProduce("", "");
-//
-//    @Test
-//    public void getFoodProducedLocallyTest() {
-//        String producedLocally = "abc";
-//        localProduce.setFoodProducedLocally(producedLocally);
-//        String expected = "abc";
-//        Assert.assertEquals(expected, localProduce.getFoodProducedLocally());
-//    }
-//
-//    @Test
-//    public void setFoodProducedLocallyTest() {
-//        String producedLocally = "abc";
-//        localProduce.setFoodProducedLocally(producedLocally);
-//        String setProducedLocally = localProduce.getFoodProducedLocally();
-//        String expected = "abc";
-//        Assert.assertEquals(expected, setProducedLocally);
-//    }
-//
-//    @Test
-//    public void getPackagedFoodTest() {
-//        String packagedFood = "abc";
-//        localProduce.setPackagedFood(packagedFood);
-//        String expected = "abc";
-//        Assert.assertEquals(expected, localProduce.getPackagedFood());
-//    }
-//
-//    @Test
-//    public void setPackagedFoodTest() {
-//        String packagedFood = "abc";
-//        localProduce.setPackagedFood(packagedFood);
-//        String setPackagedFood = localProduce.getPackagedFood();
-//        String expected = "abc";
-//        Assert.assertEquals(expected, setPackagedFood);
-//    }
-//
-//    @Test
-//    public void toStringTest() {
-//        LocalProduce localProduce = new LocalProduce("abc", "def");
-//        String expected = "LocalProduce{foodProducedLocally='def', packagedFood='abc'}";
-//        Assert.assertEquals(localProduce.toString(), expected);
-//    }
-//
-//    @Test
-//    public void equalsMethodSameObjectTest() {
-//        localProduce.setPackagedFood("abc");
-//        localProduce.setFoodProducedLocally("def");
-//        localProduce2 = localProduce;
-//        Assert.assertEquals(localProduce, localProduce2);
-//    }
-//
-//    @Test
-//    public void equalsMethodDifferentObjectTest() {
-//        localProduce.setFoodProducedLocally("abc");
-//        localProduce.setPackagedFood("def");
-//        localProduce2.setFoodProducedLocally("abc");
-//        localProduce2.setPackagedFood("def");
-//        Assert.assertEquals(localProduce, localProduce2);
-//    }
-//
-//    @Test
-//    public void equalsMethodDifferentOtherFoodProducedLocallyTest() {
-//        localProduce.setPackagedFood("abc");
-//        localProduce.setFoodProducedLocally("def");
-//        localProduce2.setPackagedFood("abc");
-//        localProduce2.setFoodProducedLocally("ghi");
-//        Assert.assertNotEquals(localProduce, localProduce2);
-//    }
-//
-//    @Test
-//    public void equalsMethodDifferentPackagedFoodTest() {
-//        localProduce.setPackagedFood("abc");
-//        localProduce.setFoodProducedLocally("def");
-//        localProduce2.setPackagedFood("ghi");
-//        localProduce2.setFoodProducedLocally("def");
-//        Assert.assertNotEquals(localProduce, localProduce2);
-//    }
-//}
+package server.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LocalProduceTest {
+
+    LocalProduce localProduce = new LocalProduce(0.0, 0.0);
+    LocalProduce localProduce2 = new LocalProduce(0.0, 0.0);
+
+    @Test
+    public void getFoodProducedLocallyTest() {
+        Double producedLocally = 1.2;
+        localProduce.setFoodProducedLocally(producedLocally);
+        Double setFoodProducedLocally = localProduce.getFoodProducedLocally();
+        Double expected = 1.2;
+        Assert.assertEquals(expected, setFoodProducedLocally);
+    }
+
+    @Test
+    public void setFoodProducedLocallyTest() {
+        Double producedLocally = 1.2;
+        localProduce.setFoodProducedLocally(producedLocally);
+        Double setProducedLocally = localProduce.getFoodProducedLocally();
+        Double expected = 1.2;
+        Assert.assertEquals(expected, setProducedLocally);
+    }
+
+    @Test
+    public void getPackagedFoodTest() {
+        Double packaged = 1.2;
+        localProduce.setPackagedFood(packaged);
+        Double setPackaged = localProduce.getPackagedFood();
+        Double expected = 1.2;
+        Assert.assertEquals(expected, setPackaged);
+    }
+
+    @Test
+    public void setPackagedFoodTest() {
+        Double packaged = 1.2;
+        localProduce.setPackagedFood(packaged);
+        Double setPackaged = localProduce.getPackagedFood();
+        Double expected = 1.2;
+        Assert.assertEquals(expected, setPackaged);
+    }
+
+    @Test
+    public void toStringTest() {
+        LocalProduce localProduce = new LocalProduce(1.2, 3.4);
+        String expected = "LocalProduce{foodProducedLocally=1.2, packagedFood=3.4}";
+        Assert.assertEquals(localProduce.toString(), expected);
+    }
+
+    @Test
+    public void equalsMethodSameObjectTest() {
+        localProduce.setPackagedFood(1.2);
+        localProduce.setFoodProducedLocally(3.4);
+        localProduce2 = localProduce;
+        Assert.assertEquals(localProduce, localProduce2);
+    }
+
+    @Test
+    public void equalsMethodDifferentObjectTest() {
+        localProduce.setFoodProducedLocally(1.2);
+        localProduce.setPackagedFood(3.4);
+        localProduce2.setFoodProducedLocally(1.2);
+        localProduce2.setPackagedFood(3.4);
+        Assert.assertEquals(localProduce, localProduce2);
+    }
+
+    @Test
+    public void equalsMethodDifferentOtherFoodProducedLocallyTest() {
+        localProduce.setPackagedFood(1.2);
+        localProduce.setFoodProducedLocally(3.4);
+        localProduce2.setPackagedFood(1.2);
+        localProduce2.setFoodProducedLocally(5.6);
+        Assert.assertNotEquals(localProduce, localProduce2);
+    }
+
+    @Test
+    public void equalsMethodDifferentPackagedFoodTest() {
+        localProduce.setPackagedFood(1.2);
+        localProduce.setFoodProducedLocally(3.4);
+        localProduce2.setPackagedFood(5.6);
+        localProduce2.setFoodProducedLocally(3.4);
+        Assert.assertNotEquals(localProduce, localProduce2);
+    }
+}
