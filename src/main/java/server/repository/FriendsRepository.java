d4b07b8c71852a4e9f47c6c97f61d3acd8ca5172
MerelSusanna <merelsusanna@gmail.com>
2019-03-22 16:42:56 +0100

    deleting and updating query for friends

--- src/main/java/server/repository/FriendsRepository.java
+++ src/main/java/server/repository/FriendsRepository.java
@@ -1,11 +1,13 @@
 package server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import server.model.EmissionFriend;
 import server.model.Friends;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -21,4 +23,13 @@ public interface FriendsRepository extends JpaRepository<Friends, Long> {
             "WHERE f.relatedUserId = ?1 AND f.relationshipType = '3'")
     List<Friends> getFriendRequestRecieved(Long relatedUserId);
 
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Friends f SET f.relationshipType = ?1 WHERE f.relatingUserId = ?2 AND f.relatedUserId = ?3")
+    void updateRelationshipType(String relationshipType, Long relatingUserId, Long relatedUserId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM Friends f WHERE f.relatingUserId = ?1 AND r.relatedUserId = ?2")
+    void deleteFriend(Long relatingUserId, Long relatedUserId);
 }
