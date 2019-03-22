9bff806f551c8c701e84be74728307a5d9b241d0
MerelSusanna <merelsusanna@gmail.com>
2019-03-22 16:44:38 +0100

    change url add friend

--- src/main/java/client/Url.java
+++ src/main/java/client/Url.java
@@ -12,7 +12,7 @@ public enum Url {
     GET_EMISSION_FRIENDS("http://localhost:8080/rest/user/get/friends/emission"),
     GET_EMISSION_USER("http://localhost:8080/rest/user/get/all/emissions"),
     GET_TOP_FRIENDS_EMISSION("http://localhost:8080/rest/user/get/top/friends/emission"),
-    ADD_FRIEND("http://localhost:8080/rest/add/friend"),
+    ADD_FRIEND("http://localhost:8080/rest/user/add/friend"),
     GET_FRIEND_REQ_SENT("http://localhost:8080/rest/user/get/friend/request/sent"),
     GET_FRIEND_REQ_REC("http://localhost:8080/rest/user/get/friend/reuqest/received");
 
