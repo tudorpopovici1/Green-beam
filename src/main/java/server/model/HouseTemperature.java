b5083713c488ebc2c08dab3adb84f5e61553b214
Maria <maria.kusmierowska@yahoo.com>
2019-03-25 12:18:45 +0100

    added the houseTemperature class with a new parameter

--- src/main/java/server/model/HouseTemperature.java
+++ src/main/java/server/model/HouseTemperature.java
@@ -1,36 +1,46 @@
 package server.model;
 
-import java.util.Objects;
-
 public class HouseTemperature {
-    private double houseTemperature;
+    private double houseTemperatureBefore;
+    private double getHouseTemperatureAfter;
 
-    public HouseTemperature(double houseTemperature) {
-        this.houseTemperature = houseTemperature;
+    public HouseTemperature(double houseTemperatureBefore, double getHouseTemperatureAfter) {
+        this.houseTemperatureBefore = houseTemperatureBefore;
+        this.getHouseTemperatureAfter = getHouseTemperatureAfter;
     }
 
     public HouseTemperature(){}
 
-    public double getHouseTemperature() {
-        return houseTemperature;
+    public double getHouseTemperatureBefore() {
+        return houseTemperatureBefore;
     }
 
-    public void setHouseTemperature(double houseTemperature) {
-        this.houseTemperature = houseTemperature;
+    public void setHouseTemperatureBefore(double houseTemperatureBefore) {
+        this.houseTemperatureBefore = houseTemperatureBefore;
+    }
+
+    public double getGetHouseTemperatureAfter() {
+        return getHouseTemperatureAfter;
+    }
+
+    public void setGetHouseTemperatureAfter(double getHouseTemperatureAfter) {
+        this.getHouseTemperatureAfter = getHouseTemperatureAfter;
     }
 
     @Override
     public String toString() {
         return "HouseTemperature{"
-                + "houseTemperature=" + houseTemperature
+                + "houseTemperatureBefore=" + houseTemperatureBefore
+                + ", getHouseTemperatureAfter=" + getHouseTemperatureAfter
                 + '}';
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        HouseTemperature that = (HouseTemperature) o;
-        return Double.compare(that.houseTemperature, houseTemperature) == 0;
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        HouseTemperature that = (HouseTemperature) object;
+        return Double.compare(that.houseTemperatureBefore, houseTemperatureBefore) == 0 &&
+                Double.compare(that.getHouseTemperatureAfter, getHouseTemperatureAfter) == 0;
     }
 }
